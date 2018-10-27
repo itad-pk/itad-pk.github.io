f2e21490b5d2bbb3f21d25872bf0c57d18e3ad9f
Arek Kliś <arek9513@gmail.com>
2018-10-27 20:12:47 +0200

    added homepage

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/itad/precache-manifest.3d4e003a55a5c5c5d1a4e8f1e351db30.js"
+  "/precache-manifest.21d66b95c1e833e909dfde64e15b2022.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/itad/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
