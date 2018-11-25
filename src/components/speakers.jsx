ab971e2bd4174f518734dc1ab6607f9068bc3b09
Arek Kliś <arek9513@gmail.com>
2018-11-25 23:24:27 +0100

    added blazej

--- src/components/speakers.jsx
+++ src/components/speakers.jsx
@@ -31,13 +31,13 @@ class Speakers extends Component {
                 alt="img"
                 width={200}
                 className="img-fluid mb-3"
-                src="http://carerealtygroup.org/wp-content/uploads/2017/04/img-headshot-placeholder-male.jpg"
+                src={require("../photos/blazej.png")}
               />
             </View>
             <div className="col-sm-6">
-              <h4>John Doe</h4>
-              <h5>Firma IT</h5>
-              <h6>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h6>
+              <h4>Błażej Drobniuch</h4>
+              <h5>NOVOMATIC</h5>
+              <h6>Agile Coach w NOVOMATIC Technologies Poland S.A. Od 2011 związany z branżą IT. Entuzjasta Turkusowych Organizacji, Large Scale Scrum (LeSS), Management 3.0, oraz ruchu #NoEstimate.</h6>
             </div>
           </div>
           <div className="row justify-content-center mb-3">
