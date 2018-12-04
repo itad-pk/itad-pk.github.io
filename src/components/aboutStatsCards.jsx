5eff727a9a1358dae8cd33083e0e1be101f23cdd
Arek Kliś <arek9513@gmail.com>
2018-12-04 10:07:10 +0100

    updated countdown

--- src/components/aboutStatsCards.jsx
+++ src/components/aboutStatsCards.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card, CardTitle, CardText } from "mdbreact";
+import { Card, CardTitle, CardText, CardBody } from "mdbreact";
 import Countdown from 'react-countdown-now';
 
 class AboutStatsCards extends Component {
@@ -18,9 +18,12 @@ class AboutStatsCards extends Component {
             boxShadow: "0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.60)"
           }}
         >
-          <CardTitle style={{ color: "white" }}>
-             Konferencja już trwa :)
-          </CardTitle>
+          <CardBody className="mt-5" style={{ color: "white" }}>
+             <h2>
+                Konferencja już trwa 
+             </h2>
+             <h1>:)</h1>
+          </CardBody>
         </Card>
         <Card
           className="p-3 text-center col-6"
