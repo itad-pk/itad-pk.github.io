6514d7b98e4d25fd64cd998465ed4cf1ac864e36
Jarosław Leśniak <jarek9244@gmail.com>
2019-12-11 12:26:25 +0100

    Optimized images

--- src/components/itad.jsx
+++ src/components/itad.jsx
@@ -13,10 +13,13 @@ export default class Itad extends React.Component {
               { id: 3, path: require("../photos/3.JPG") },
               { id: 4, path: require("../photos/4.JPG") },
               { id: 5, path: require("../photos/5.JPG") },
-              { id: 6, path: require("../photos/6.JPG") },
+              { id: 6, path: require("../photos/11.JPG") },
               { id: 7, path: require("../photos/7.JPG") },
               { id: 8, path: require("../photos/8.JPG") },
-              { id: 9, path: require("../photos/9.JPG") }
+              { id: 9, path: require("../photos/9.JPG") },
+              { id: 10, path: require("../photos/10.JPG") },
+              { id: 11, path: require("../photos/12.JPG") },
+              { id: 12, path: require("../photos/6.JPG") }
             ]
           };
     }
