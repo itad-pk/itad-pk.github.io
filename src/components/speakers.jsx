909ffb08b0785ceedbe8b12d0ee2b93562604efb
Jarosław Leśniak <jarek9244@gmail.com>
2020-01-07 18:31:31 +0100

    Added speaker

--- src/components/speakers.jsx
+++ src/components/speakers.jsx
@@ -10,7 +10,8 @@ export default class Speakers extends React.Component {
                     <h2>Prelegenci</h2>
                     <MDBRow>
                         <Speaker name="Paweł Mstowski" img="Paweł_Mstowski.jpg" desc="Paweł to Senior Software Developer w ABB. Podczas swojej prelekcji opowie o rozproszonym uwierzytelnianiu użytkowników z użyciem OpenId Connect w aplikacjach webowych"/>
-                        <Speaker name="Dominik Pawlik" img="Dominik_Pawlik.jpg" desc="Dominik pracuje jako PHP Developer w Ailleronie. Podczas swojej prelekcji opowie o dobrych nawykach pisania kodu oraz udzieli kilku wskazówek, dzięki którym nie zgubicie się we własnym kodzie."/>
+                        <Speaker name="Dominik Pawlik" img="Dominik_Pawlik.jpg" desc="Dominik pracuje jako PHP Developer w Ailleronie. Podczas swojej prelekcji opowie o dobrych nawykach pisania kodu oraz udzieli kilku wskazówek, dzięki którym się w nim nie zgubicie!"/>
+                        <Speaker name="Katarzyna Olszówka" img="Katarzyna_Olszowka.jpg" desc="Kasia pracuje jako Specjalistka ds. Rozwoju Ludzi i Organizacji w firmie Ailleron. W swojej prelekcji opowie o sposobach rozwoju zawodowego."/>
                     </MDBRow>
                     <p className="soon">Już wkrótce przedstawimy kolejnych prelegentów</p>
                 </MDBContainer>
