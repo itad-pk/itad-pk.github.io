af044d70f5d9bed39f86c936cb5ab853be14657a
Jarosław Leśniak <jarek9244@gmail.com>
2020-01-14 00:14:51 +0100

    Adde speakers

--- src/components/speakers.jsx
+++ src/components/speakers.jsx
@@ -12,6 +12,12 @@ export default class Speakers extends React.Component {
                         <Speaker name="Paweł Mstowski" img="Paweł_Mstowski.jpg" desc="Paweł to Senior Software Developer w ABB. Podczas swojej prelekcji opowie o rozproszonym uwierzytelnianiu użytkowników z użyciem OpenId Connect w aplikacjach webowych"/>
                         <Speaker name="Dominik Pawlik" img="Dominik_Pawlik.jpg" desc="Dominik pracuje jako PHP Developer w Ailleronie. Podczas swojej prelekcji opowie o dobrych nawykach pisania kodu oraz udzieli kilku wskazówek, dzięki którym się w nim nie zgubicie!"/>
                         <Speaker name="Katarzyna Olszówka" img="Katarzyna_Olszowka.jpg" desc="Kasia pracuje jako Specjalistka ds. Rozwoju Ludzi i Organizacji w firmie Ailleron. W swojej prelekcji opowie o sposobach rozwoju zawodowego."/>
+                        <Speaker name="Łukasz Świszcz" img="Lukasz_Swiszcz.png" desc='Łukasz pracuje jako pracuje jako Technician Team Leader w firmie WEBCON. Podczas swojej prelekcji przedstawi rozwiązania firmy Microsoft. Tematem wykładu Łukasza jest "Azure Cognitive Services – inteligencja w chmurze Microsoft".'/>
+                    </MDBRow>
+                    <MDBRow>
+                        <Speaker name="Jakub Bobek" img="Jakub_Bobek.png" desc="Kuba pracuje jako Software Developer w firmie NOVOMATIC Technologies. Podczas swojej prelekcji pokaże jak stworzyć paczkę, z której skorzysta ktoś inny."/>
+                        <Speaker name="Konrad Bielawski" img="Konrad_Bielawski.png" desc="Konrad pracuje jako Software Engineer w firmie NOVOMATIC Technologies. Podczas swojej prelekcji poruszy temat DevOps'u i pokaże jak zautomatyzować swój kod."/>
+                        <Speaker name="Andrzej Kubik" img="Andrzej_Kubik.png" desc="Andrzej pracuje jako Product Manager w firmie Motorola Solutions. Podczas swojej prelekcji opowie o  bezpieczeństwo danych w chmurze z perspektywy klienta."/>
                     </MDBRow>
                     <p className="soon">Już wkrótce przedstawimy kolejnych prelegentów</p>
                 </MDBContainer>
