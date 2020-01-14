08556b43cb6857a4150f3ea37324a665eada15bf
Jarosław Leśniak <jarek9244@gmail.com>
2020-01-14 22:58:10 +0100

    Change speakers description

--- src/components/speakers.jsx
+++ src/components/speakers.jsx
@@ -15,8 +15,8 @@ export default class Speakers extends React.Component {
                         <Speaker name="Łukasz Świszcz" img="Lukasz_Swiszcz.png" desc='Łukasz pracuje jako pracuje jako Technician Team Leader w firmie WEBCON. Podczas swojej prelekcji przedstawi rozwiązania firmy Microsoft. Tematem wykładu Łukasza jest "Azure Cognitive Services – inteligencja w chmurze Microsoft".'/>
                     </MDBRow>
                     <MDBRow>
-                        <Speaker name="Jakub Bobek" img="Jakub_Bobek.png" desc="Kuba pracuje jako Software Developer w firmie NOVOMATIC Technologies Poland. Podczas swojej prelekcji pokaże jak stworzyć paczkę, z której skorzysta ktoś inny."/>
-                        <Speaker name="Konrad Bielawski" img="Konrad_Bielawski.png" desc="Konrad pracuje jako Software Engineer w firmie NOVOMATIC Technologies Poland. Podczas swojej prelekcji poruszy temat DevOps'u i pokaże jak zautomatyzować swój kod."/>
+                        <Speaker name="Jakub Bobek" img="Jakub_Bobek.png" desc="Programista działający głównie z .NET. Pracuje w firmie NOVOMATIC Technologies Poland, gdzie rozwija i utrzymuje rozproszony system wspierający branżę gamingową."/>
+                        <Speaker name="Konrad Bielawski" img="Konrad_Bielawski.png" desc="Software Engineer w firmie NOVOMATIC Technologies Poland. W swojej prelekcji opowie o tym jak kultura DevOps może pomóc z prywatnym projektem."/>
                         <Speaker name="Andrzej Kubik" img="Andrzej_Kubik.png" desc="Andrzej pracuje jako Product Manager w firmie Motorola Solutions. Podczas swojej prelekcji opowie o  bezpieczeństwo danych w chmurze z perspektywy klienta."/>
                         <Speaker name="Błażej Drobniuh" img="Blazej_Drobniuh.png" desc="Błażej to Agile Coach pracujący w firmie Strix. Podczas spotkania pomoże znaleźć odpowiedzi na następujące pytania: Co odróżnia pewne organizacje od innych? Jaki wpływ na Twoją karierę i życie mogą mieć te różnice? Jak podczas rekrutacji rozpoznać, czy dana organizacja jest odpowiednia dla Ciebie?"/>
                     </MDBRow>
